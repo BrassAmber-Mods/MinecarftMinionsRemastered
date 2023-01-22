795d245941e91e5defc4715d1b95f7a3c1b0960b
jodi / jodlodi <jodlodi090@gmail.com>
2023-01-22 17:14:04 +0100

    Initial commit

---
+++ src/main/java/io/github/jodlodi/minions/MinionsRemake.java
@@ -0,0 +1,27 @@
+package io.github.jodlodi.minions;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(MinionsRemake.MODID)
+public class MinionsRemake
+{
+    public static final String MODID = "minions_remake";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public MinionsRemake() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+
+    }
+}
