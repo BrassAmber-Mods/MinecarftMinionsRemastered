ea2330bfee26a9f4dc8240785dc61261bee1fc5f
jodi / jodlodi <jodlodi090@gmail.com>
2023-07-08 04:24:12 +0200

    Mining speed now behaves like a player's

--- src/main/java/io/github/jodlodi/minions/minion/Minion.java
+++ src/main/java/io/github/jodlodi/minions/minion/Minion.java
@@ -18,9 +18,12 @@ import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.players.OldUsersConverter;
+import net.minecraft.tags.FluidTags;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.effect.MobEffectUtil;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.OwnableEntity;
@@ -33,6 +36,7 @@ import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.DyeItem;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.phys.AABB;
@@ -48,6 +52,7 @@ import org.jetbrains.annotations.Nullable;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -325,8 +330,34 @@ public class Minion extends PathfinderMob implements OwnableEntity {
         return this.getBlinking() || super.isInvisible();
     }
 
+    @SuppressWarnings("deprecation")
     public float getMineSpeed() {
-        return 3.75F;//FIXME
+        float speed = 3.75F;
+
+        if (MobEffectUtil.hasDigSpeed(this)) {
+            speed *= 1.0F + (float)(MobEffectUtil.getDigSpeedAmplification(this) + 1) * 0.2F;
+        }
+
+        if (this.hasEffect(MobEffects.DIG_SLOWDOWN)) {
+            float f1 = switch (Objects.requireNonNull(this.getEffect(MobEffects.DIG_SLOWDOWN)).getAmplifier()) {
+                case 0 -> 0.3F;
+                case 1 -> 0.09F;
+                case 2 -> 0.0027F;
+                default -> 8.1E-4F;
+            };
+
+            speed *= f1;
+        }
+
+        if (this.isEyeInFluid(FluidTags.WATER) && !EnchantmentHelper.hasAquaAffinity(this)) {
+            speed /= 5.0F;
+        }
+
+        if (!this.onGround) {
+            speed /= 5.0F;
+        }
+
+        return speed;
     }
 
     @Override
